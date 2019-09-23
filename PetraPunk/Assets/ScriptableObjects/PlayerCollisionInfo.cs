927b08808b2a2668f1b1305fd854cad3ce23631f
Nicolai <nwk1391@gmail.com>
2019-09-23 16:01:37 +0200

    Player Controller
    I added the basic controls

---
+++ PetraPunk/Assets/ScriptableObjects/PlayerCollisionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionInfo : MonoBehaviour
+{
+    public Transform PlayerTrans;
+    public PlayerController playerController;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+        if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            playerController.GetHit(collision.GetContact(0).normal);
+
+            
+
+
+        }
+    }
+}
