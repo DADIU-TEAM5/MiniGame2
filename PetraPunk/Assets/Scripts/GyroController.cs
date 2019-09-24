f41f114f3e83042df6b2d873722083e612e0ee40
Martin Krøll <martino1215@hotmail.com>
2019-09-24 09:30:54 +0200

    Added Gyro

---
+++ PetraPunk/Assets/Scripts/GyroController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GyroController : MonoBehaviour
+{
+    public Text text;
+    public float steeringInput;
+    public float maxVelocity;
+
+    //public Text tex;
+
+
+    float timer = 0;
+
+    float rotationThreshHold = 0.2f;
+    float currentRot = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+        Input.gyro.enabled = true;
+        //transform.Rotate(Vector3.up * 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotRate = Input.gyro.rotationRate.z;
+        currentRot += rotRate * Time.deltaTime;
+
+        
+        float cappedRotation = 
+
+        steeringInput = currentRot;
+        Debug.Log(steeringInput);
+        string temp = "Steering value:  " + steeringInput;
+        text.text = temp;
+
+
+        
+       
+        //transform.Rotate(Vector3.up * Mathf.Min(steeringInput * 100, maxVelocity) * Time.deltaTime);
+        transform.Translate(Vector3.left * steeringInput * 10 * Time.deltaTime);
+
+        //if (Mathf.Abs(currentRot) > rotationThreshHold)
+        //{
+        //    //timer = 0;
+        //    if (currentRot < 0)
+        //    {
+        //        steeringInput = -1;
+        //    }
+        //    else
+        //    {
+
+        //        steeringInput = 1;
+        //    }
+        //}
+        //else
+        //{
+
+        //    steeringInput = 0;
+
+        //}
+
+
+    }
+}
