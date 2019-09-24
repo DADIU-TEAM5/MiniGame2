26a42b92ff9c9ff060d984cc3bf9fa10300009b3
Martin Krøll <martino1215@hotmail.com>
2019-09-24 10:40:13 +0200

    Gyro Toggle

--- PetraPunk/Assets/Scripts/GyroController.cs
+++ PetraPunk/Assets/Scripts/GyroController.cs
@@ -5,45 +5,66 @@ using UnityEngine.UI;
 
 public class GyroController : MonoBehaviour
 {
-    public float steeringInput;
+
+    private float gyroInput;
+    public float speedMultiplier =10;
     public float maxVelocity;
 
+    public enum control { New, Old };
 
-    public FloatVariable steeringInputFloat;
+    public control controlType;
+
+
+    public FloatVariable steeringOutput;
 
     //public Text tex;
 
 
-    float timer = 0;
+    // float timer = 0;
 
-    float rotationThreshHold = 0.2f;
+    // float rotationThreshHold = 0.2f;
     float currentRot = 0;
-    // Start is called before the first frame update
+
+
+
     void Start()
     {
         Screen.orientation = ScreenOrientation.Landscape;
         Input.gyro.enabled = true;
-        //transform.Rotate(Vector3.up * 30);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rotRate = Input.gyro.rotationRate.z;
+        float rotRate = 0;
+
+        if (controlType == control.Old)
+        {
+            rotRate = Input.gyro.rotationRate.y;
+        }
+        else
+        {
+            rotRate = Input.gyro.rotationRate.z;
+        }
+
+
         currentRot += rotRate * Time.deltaTime;
 
+
+        gyroInput = currentRot * -speedMultiplier;
         
-        float cappedRotation = 
-
-        steeringInput = currentRot *-10f;
-        Debug.Log(steeringInput);
 
 
 
-        steeringInputFloat.Value = steeringInput;
+        steeringOutput.Value = gyroInput;
+       
+        
+        // Dampening
         //transform.Rotate(Vector3.up * Mathf.Min(steeringInput * 100, maxVelocity) * Time.deltaTime);
-       // transform.Translate(Vector3.left * steeringInput * 10 * Time.deltaTime);
+        // transform.Translate(Vector3.left * steeringInput * 10 * Time.deltaTime);
 
+        // Old Threshold
         //if (Mathf.Abs(currentRot) > rotationThreshHold)
         //{
         //    //timer = 0;
