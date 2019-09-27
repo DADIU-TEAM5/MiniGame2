68fc770a4760b90e0fb5d92f1b9f7ea4ecc038a8
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-27 09:58:58 +0200

    Audiovariable

---
+++ PetraPunk/Assets/Scripts/Scriptable Objects/AudioVariable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AudioVariable : ScriptableObject
+{
+    public AK.Wwise.Event AudioEvent;
+
+    public AK.Wwise.Bank Bank;
+
+    public void PostEvent(GameObject source) {
+        AudioEvent.Post(source);
+    }
+}
